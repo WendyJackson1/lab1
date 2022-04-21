f1ba3c4ddb25bc94e9194c08107789ea8f72d734
WendyJackson1 <wendyrobinh@gmail.com>
2022-04-21 10:23:48 -0400

    this is a test run

--- roomCalculator/roomCalculator/Program.cs
+++ roomCalculator/roomCalculator/Program.cs
@@ -1,8 +1,10 @@
 ﻿public class Program
 {
+
     public static void Main()
     {
-        
+        int area = 0;
+
         Console.WriteLine("Please enter length of the room");
         string inputOne = Console.ReadLine();
         double length = double.Parse(inputOne);
@@ -16,17 +18,26 @@
         Console.WriteLine("Please enter hight of the room");
         string inputThree = Console.ReadLine();
         double height = double.Parse(inputThree);
-        Console.WriteLine("Your room is "+ height + " in height.");
-
-        double perimeter = 2 * (length + width); 
-        Console.WriteLine("Your rooms perimeter is " + perimeter);
-        double area = width * length;
-        Console.WriteLine("Your rooms area is " + area);
+        Console.WriteLine("Your room is " + height + " in height.");
 
         double roomSurface = 2 * (width * length + height * length + height * width);
         Console.WriteLine("Your rooms surface area is " + roomSurface);
-        double roomVolume = length * width * height;
-        Console.WriteLine("Your rooms volume area is " + roomVolume);
+
+        GetPerimeter(length, width);
+
+        GetArea(length, height, width);
+
+        GetVolume(length, height, width);
+
+        //perimeter
+        //double perimeter = 2 * (length + width); 
+        //Console.WriteLine("Your rooms perimeter is " + perimeter);
+        //area
+        //double area = width * length;
+        //Console.WriteLine("Your rooms area is " + area);
+        //volume
+        //double roomVolume = length * width * height;
+        //Console.WriteLine("Your rooms volume area is " + roomVolume);
 
         switch (area)
         {
@@ -42,4 +53,26 @@
 
         }
     }
+
+    public static double GetPerimeter(double length, double width)
+    {
+        double perimeter = 2 * (length + width);
+        Console.WriteLine("Your rooms perimeter is " + perimeter);
+        return perimeter;
+
+    }
+
+    public static double GetArea(double length, double height, double width)
+    {
+        double area = width * length;
+        Console.WriteLine("Your rooms area is " + area);
+        return area;
+    }
+    public static double GetVolume(double length, double width, double height)
+    {
+        double roomVolume = length * width * height;
+        Console.WriteLine("Your rooms volume area is " + roomVolume);
+        return roomVolume;
+    }
 }
+
